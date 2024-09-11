926742a48bb3f7031fa2befea9a5b06eb01cd965
Anh Nguyen <ngoc20022607@gmail.com>
2024-09-11 08:22:19 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nguyen_BT02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student(1, "Tran Van A", 15));
+            students.Add(new Student(2, "Tran Van B", 16));
+            students.Add(new Student(3, "Tran Van C", 17));
+            students.Add(new Student(4, "Tran Van D", 18));
+            students.Add(new Student(5, "Tran Van E", 19));
+
+            Console.WriteLine("a ========================");
+            print(students);
+            Console.WriteLine("b ========================");
+
+            From15to18(students);
+            Console.WriteLine("c ========================");
+
+            NameStartWithA(students);
+            Console.WriteLine("d ========================");
+
+            SumAge(students);
+            Console.WriteLine("e ========================");
+
+            FindAgeMax(students);
+            Console.WriteLine("f ========================");
+
+            OrderByAgeASC(students);
+            Console.ReadLine();
+        }
+
+
+        public static void print(List<Student> list)
+        {
+            foreach (Student student in list)
+            {
+                Console.WriteLine(student.Id);
+                Console.WriteLine(student.Name);
+                Console.WriteLine(student.Age);
+            }
+
+
+        }
+
+
+        public static void From15to18(List<Student> list)
+        {
+            List<Student> students = list.Where(student => student.Age >= 15 && student.Age <= 18).ToList();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Id);
+                Console.WriteLine(student.Name);
+                Console.WriteLine(student.Age);
+            }
+
+        }
+
+        public static void NameStartWithA(List<Student> list)
+        {
+            List<Student> students = list
+                .Where(student =>
+                    student.Name.Trim().Substring(student.Name.LastIndexOf(" ") + 1)
+                        .StartsWith("A"))
+                .ToList();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Id);
+                Console.WriteLine(student.Name);
+                Console.WriteLine(student.Age);
+            }
+        }
+
+        public static void SumAge(List<Student> list)
+        {
+            Console.WriteLine(list.Sum(stu => stu.Age));
+        }
+
+        public static void FindAgeMax(List<Student> list)
+        {
+            Student result = list.OrderByDescending(student => student.Age).FirstOrDefault();
+            if (result != null)
+            {
+                Console.WriteLine(result.Id);
+                Console.WriteLine(result.Name);
+                Console.WriteLine(result.Age);
+            }
+        }
+
+        public static void OrderByAgeASC(List<Student> list)
+        {
+            print(list.OrderBy(student => student.Age).ToList());
+
+        }
+
+
+    }
+
+
+}
---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nguyen_BT02
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(int id, string ten, int age)
+        {
+            Id = id;
+            Name = ten;
+            Age = age;
+        }
+    }
+}
